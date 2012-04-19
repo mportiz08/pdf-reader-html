1f700c0ab02f7b9758052c798b711bf25f8a8b71
Marcus Ortiz <mportiz08@gmail.com>
2012-04-18 17:30:45 -0700

    to_html

--- lib/pdf/reader/html.rb
+++ lib/pdf/reader/html.rb
@@ -1,11 +1,24 @@
 require 'pdf/reader'
+require 'html_writer'
 
 require 'pdf/reader/html/version'
 
 module PDF
   class Reader
-    def to_html
-      'hello, world'
+    class Page
+      def to_html
+        self.text
+      end
+    end
+    
+    def to_html(options={})
+      HtmlWriter.new.write do |html|
+        html.doctype options[:doctype] || 5
+        html.head do |head|
+          head.title options[:title] || ''
+        end
+        html.body self.pages.map(&:to_html)
+      end
     end
   end
 end
